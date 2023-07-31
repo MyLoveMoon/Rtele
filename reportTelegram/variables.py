af28a1bd65e777db6f53beb39d7744a9c44b8a16
ᴍʏʟᴏᴠᴇᴍᴏᴏɴ <90930470+MyLoveMoon@users.noreply.github.com>
2023-07-31 12:26:54 +0530

    Update variables.py

--- reportTelegram/variables.py
+++ reportTelegram/variables.py
@@ -3,10 +3,10 @@ import configparser
 config = configparser.ConfigParser()
 config.read('config.ini')
 
-LINK=int(config['Telegram']['https://t.me/treportest'])
-group_id = int(config['Telegram']['-1001824856185'])
-admin_id = int(config['Telegram']['6056591125'])
-sticker = int(config['Telegram']['CAACAgEAAxkBAAEJ3K5kx0JvDnWjIIWq__9YIiTH7dWszwAC6AIAAuWAwUS8QzStENdBxy8E'])
+LINK=int(config['https://t.me/treportest'])
+GROUP_ID=int(config['-1001824856185'])
+ADMIN_ID=int(config['6056591125'])
+STICKER=int(config['CAACAgEAAxkBAAEJ3K5kx0JvDnWjIIWq__9YIiTH7dWszwAC6AIAAuWAwUS8QzStENdBxy8E'])
 
 
 DB_HOST = config['Database']['DB_HOST']
